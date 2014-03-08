b586558fe33afa3db64f6a74b4933fa3c6fa4fb7
Administrator <Administrator@USER-20130226GL>
2014-03-08 17:44:16 +0800

    前台框架修改基本完成

--- src/com/shopping/web/action/UserAction.java
+++ src/com/shopping/web/action/UserAction.java
@@ -8,31 +8,31 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class UserAction {
 	@RequestMapping("/index.do")
 	public String index() {
-		return "index";
+		return "front/index";
 	}
 	
 	@RequestMapping("/good_list.do")
 	public String goodList() {
-		return "good_list";
+		return "front/good_list";
 	}
 
 	@RequestMapping("/good_detail.do")
 	public String goodDetail() {
-		return "good_detail";
+		return "front/good_detail";
 	}
 
 	@RequestMapping("/shop_cart.do")
 	public String shopCart() {
-		return "good_detail";
+		return "front/good_detail";
 	}
 
 	@RequestMapping("/contact.do")
 	public String Contact() {
-		return "contact";
+		return "front/contact";
 	}
 
 	@RequestMapping("/buy.do")
 	public String buy() {
-		return "buy";
+		return "front/buy";
 	}
 }
